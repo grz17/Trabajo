9c0f7828a533f2071cc1dd856f5c1e36507a4006
grz1708 <resendizgabriel19@gmail.com>
2021-02-21 23:45:04 -0600

    inicio

---
+++ app/src/main/java/com/example/newmoviezone/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.newmoviezone;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn =(Button)findViewById(R.id.btnNecesitasUnaCuenta);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),RegistrarUsuario.class);
+                startActivity(intent);
+            }
+        });
+        Button bto =(Button)findViewById(R.id.btnIniciarSesion);
+        bto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),EntradaMovieZone.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/newmoviezone/RegistrarUsuario.java
@@ -0,0 +1,25 @@
+package com.example.newmoviezone;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RegistrarUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_usuario);
+
+        Button btn =(Button)findViewById(R.id.btnYatengocuenta);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
